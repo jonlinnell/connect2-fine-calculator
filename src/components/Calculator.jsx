9dfbad6e0d97900438eb621879a75a993a32313e
Jon Linnell <linnell.jon@gmail.com>
2019-03-26 17:27:36 +0000

    Style tweeks

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -2,9 +2,11 @@ import React, { useCallback, useState } from 'react';
 import parse from 'csv-parse';
 import moment from 'moment-business-days';
 
-import fines from '../misc/fines.json'
+import fines from '../misc/fines.json';
 
 moment.locale('en-GB');
+
+const tableStyles = { maxHeight: '1000px', width: '90vw', overflow: 'scroll', margin: '1rem' };
 
 const formatDate = date => moment(date).format('LLL');
 
@@ -13,22 +15,26 @@ const Calculator = () => {
   const [totalRecords, setTotalRecords] = useState(0);
 
   const processOverdueRecords = useCallback(records => {
-    setOverdueLoans(records.map(record => {
-      const dailyFine = fines.find(fine => (fine.resource === record['Parent resource'] || fine.resource === record.Resource)).amount || 0;
+    setOverdueLoans(
+      records.map(record => {
+        const dailyFine =
+          fines.find(
+            fine => fine.resource === record['Parent resource'] || fine.resource === record.Resource
+          ).amount || 0;
 
-      return {
-        daysOverdue: moment(record['End date']).businessDiff(moment(record['Checked in'])),
-        dailyFine,
-        ...record
-      }
-    }));
+        return {
+          daysOverdue: moment(record['End date']).businessDiff(moment(record['Checked in'])),
+          dailyFine,
+          ...record,
+        };
+      })
+    );
   }, []);
 
   const handleFile = useCallback(file => {
     const reader = new FileReader();
     reader.onload = () => {
-      const fileContents = reader.result
-        .replace('Barcode', 'Resource Barcode');
+      const fileContents = reader.result.replace('Barcode', 'Resource Barcode');
 
       parse(
         fileContents,
@@ -43,7 +49,7 @@ const Calculator = () => {
 
           const overdue = output
             .filter(row => moment(row['Checked in']).isAfter(moment(row['End date']))) // overdue loans only
-            .filter(row => (/p|u/.test(row['SAML Field (Type)']))) // PGTs and UGs only
+            .filter(row => /p|u/.test(row['SAML Field (Type)'])) // PGTs and UGs only
             .filter(row => moment(row['End date']).businessDiff(moment(row['Checked in'])) > 0); // same-day grace period
 
           setTotalRecords(output.length);
@@ -56,15 +62,15 @@ const Calculator = () => {
   }, []);
 
   return (
-    <div>
+    <div style={{ textAlign: 'center' }}>
       <input type="file" name="file" onChange={e => handleFile(e.target.files[0])} />
       {overdueLoans.length ? (
         <p>
           {overdueLoans.length} of {totalRecords} items overdue.
         </p>
       ) : null}
-      <div style={{ maxHeight: '1400px', width: '80vw', overflow: 'scroll' }}>
-        <table className="pure-table">
+      <div style={tableStyles}>
+        <table className="pure-table" style={{ width: '100%' }}>
           <thead>
             <tr>
               <th>Booking reference</th>
@@ -82,17 +88,24 @@ const Calculator = () => {
           <tbody>
             {overdueLoans.map(loan => (
               <tr key={`${loan['Ref no']}-${loan['Resource Barcode']}`}>
-                <td><a href={`https://lborolondon.getconnect2.com/bookings/view.aspx?id=${loan['Ref no'].slice(-6)}`}>{loan['Ref no']}</a></td>
-                <td>{loan['First name']} {loan.Surname}</td>
+                <td>
+                  <a
+                    href={`https://lborolondon.getconnect2.com/bookings/view.aspx?id=${loan[
+                      'Ref no'
+                    ].slice(-6)}`}
+                  >
+                    {loan['Ref no']}
+                  </a>
+                </td>
+                <td>
+                  {loan['First name']} {loan.Surname}
+                </td>
                 <td>{loan.Barcode}</td>
                 <td>{loan.Email}</td>
                 <td>
-                  {
-                    loan.Barcode
-                      ? (<span>({loan['Resource Barcode']})&nbsp;</span>)
-                      : null
-                  }
-                {loan.Resource}</td>
+                  {loan.Barcode ? <span>({loan['Resource Barcode']})&nbsp;</span> : null}
+                  {loan.Resource}
+                </td>
                 <td>{formatDate(loan['End date'])}</td>
                 <td>{formatDate(loan['Checked in'])}</td>
                 <td>{loan.daysOverdue}</td>
