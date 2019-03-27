6d28b2a3582e187b9b2a0b854c23d88576927e4a
Jon Linnell <linnell.jon@gmail.com>
2019-03-27 17:37:15 +0000

    Vanity

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,6 +9,7 @@ const Home = () => (
   <FullPageSection>
     <SectionTitle>Late fee calculator</SectionTitle>
     <Calculator />
+    <footer style={{ fontSize: '0.8rem', margin: '12px 0' }}>2019 Jon Linnell, Loughborough University London</footer>
   </FullPageSection>
 );
 
